bc02a0a9cae92e98a06e744a4ad6d8553afc520d
Mateusz Kłakus <mati.klakus@gmail.com>
2023-02-02 09:24:03 +0100

    feat: updated tests for CreateAccount

--- packages/extension-ui/src/Popup/CreateAccount/CreateAccount.spec.tsx
+++ packages/extension-ui/src/Popup/CreateAccount/CreateAccount.spec.tsx
@@ -6,15 +6,14 @@
 import '@polkadot/extension-mocks/chrome';
 
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import { configure, mount, ReactWrapper, shallow } from 'enzyme';
+import { configure, mount, ReactWrapper } from 'enzyme';
 import React from 'react';
 import { act } from 'react-dom/test-utils';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
-import { AccountNamePasswordCreation, ActionContext, ActionText, Button, themes } from '../../components';
+import { AccountNamePasswordCreation, ActionContext, Button, themes } from '../../components';
 import * as messaging from '../../messaging';
-import { Header } from '../../partials';
 import HeaderWithSteps from '../../partials/HeaderWithSteps';
 import { flushAllPromises } from '../../testHelpers';
 import CreateAccount from '../CreateAccount/index';
@@ -47,20 +46,6 @@ describe('Create Account', () => {
       </ActionContext.Provider>
     );
 
-  const check = (input: ReactWrapper): unknown => input.simulate('change', { target: { checked: true } });
-
-  const type = async (input: ReactWrapper, value: string): Promise<void> => {
-    input.simulate('change', { target: { value } });
-    await act(flushAllPromises);
-    wrapper.update();
-  };
-
-  const enterName = (name: string): Promise<void> => type(wrapper.find('input').first(), name);
-  const password = (password: string) => (): Promise<void> =>
-    type(wrapper.find('input[type="password"]').first(), password);
-  const repeat = (password: string) => (): Promise<void> =>
-    type(wrapper.find('input[type="password"]').last(), password);
-
   describe('CreateAccount component', () => {
     beforeEach(async () => {
       onActionStub = jest.fn();
@@ -82,6 +67,41 @@ describe('Create Account', () => {
       wrapper.setState({ step: 1 });
 
       expect(wrapper.find(SafetyFirst).exists()).toBe(true);
+    });
+
+    it('renders SafetyFirst component after clicking back button on step 2', () => {
+      wrapper.find(Button).last().simulate('click');
+      wrapper.find(Button).at(1).simulate('click');
+      wrapper.find(Button).first().simulate('click');
+      wrapper.update();
+      console.log(wrapper.children().debug());
+      console.log(wrapper.find(Button));
+
+      expect(wrapper.find(SafetyFirst).exists()).toBe(true);
+    });
+
+    it('renders SaveMnemonic component after clicking next button on step 1', () => {
+      wrapper.find(Button).last().simulate('click');
+      wrapper.update();
+
+      expect(wrapper.find(SaveMnemonic).exists()).toBe(true);
+    });
+
+    it('renders SaveMnemonic component after clicking Back button on step 3', () => {
+      wrapper.find(Button).last().simulate('click');
+      wrapper.find(Button).last().simulate('click');
+      wrapper.find(Button).first().simulate('click');
+      wrapper.update();
+
+      expect(wrapper.find(SaveMnemonic).exists()).toBe(true);
+    });
+
+    it('renders AccountNamePasswordCreation component after clicking next button on step 2', () => {
+      wrapper.find(Button).last().simulate('click');
+      wrapper.find(Button).last().simulate('click');
+      wrapper.update();
+
+      expect(wrapper.find(AccountNamePasswordCreation).exists()).toBe(true);
     });
   });
 });
