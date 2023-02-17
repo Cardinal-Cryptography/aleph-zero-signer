7ad6c62961dd27c21de239e893e64d0c1057662f
Mateusz Kłakus <mati.klakus@gmail.com>
2023-02-17 15:54:13 +0100

    fix: fixed tests

--- packages/extension-ui/src/components/AccountNamePasswordCreation.spec.tsx
+++ packages/extension-ui/src/components/AccountNamePasswordCreation.spec.tsx
@@ -13,7 +13,7 @@ import { act } from 'react-dom/test-utils';
 import { flushAllPromises } from '../testHelpers';
 import BackButton from './BackButton';
 import Button from './Button';
-import { AccountNamePasswordCreation, Input, InputWithLabel, NextStepButton } from '.';
+import { AccountNamePasswordCreation, Input, InputWithLabel } from '.';
 
 // For this file, there are a lot of them
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
