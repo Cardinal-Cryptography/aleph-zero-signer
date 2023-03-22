69484f4a89667533d7e6b6a72c4d604db7f40a0d
Mateusz Kłakus <mati.klakus@gmail.com>
2023-03-22 17:05:16 +0100

    fix: PR fixes

--- packages/extension-ui/src/Popup/About.tsx
+++ packages/extension-ui/src/Popup/About.tsx
@@ -49,7 +49,6 @@ function About({ className }: Props): React.ReactElement<Props> {
           link={LINKS.GENERAL_INTRODUCTION}
           position='bottom'
           title={t<string>('Help & Support')}
-          tabIndex={0}
         />
         <AboutMenuCard
           description=''
@@ -57,7 +56,6 @@ function About({ className }: Props): React.ReactElement<Props> {
           link={LINKS.TERMS_OF_SERVICE}
           position='bottom'
           title={t<string>('Terms of Service')}
-          tabIndex={0}
         />
         <AboutMenuCard
           description=''
@@ -65,7 +63,6 @@ function About({ className }: Props): React.ReactElement<Props> {
           link={LINKS.PRIVACY_POLICY}
           position='bottom'
           title={t<string>('Privacy Policy')}
-          tabIndex={0}
         />
         <AboutMenuCard
           description=''
@@ -73,7 +70,6 @@ function About({ className }: Props): React.ReactElement<Props> {
           link={LINKS.MAIN_WEBSITE}
           position='bottom'
           title={t<string>('Visit Website')}
-          tabIndex={0}
         />
       </div>
     </>
