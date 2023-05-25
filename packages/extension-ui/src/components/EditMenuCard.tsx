6d5f59c8fd1156f46ed51637be580f5b8d9ed3ef
Piotr Dżaluk <piotr.dzaluk@cardinals.cc>
2023-05-25 19:25:52 +0200

    A0-2235: Handle long names in account edition view

--- packages/extension-ui/src/components/EditMenuCard.tsx
+++ packages/extension-ui/src/components/EditMenuCard.tsx
@@ -176,7 +176,7 @@ export default styled(EditMenuCard)(
 
   .flex-group {
     display: flex;
-    gap: 4px;
+    gap: 40px;
     align-content: flex-start;
     align-items: center;
     justify-content: space-between;
@@ -202,6 +202,7 @@ export default styled(EditMenuCard)(
     font-weight: 300;
     font-size: 14px;
     line-height: 145%;
+    min-width: 0;
     max-width: 230px;
     white-space: pre-line;
     gap: 14px;
@@ -211,7 +212,6 @@ export default styled(EditMenuCard)(
       white-space: nowrap;
       text-overflow: ellipsis;
       overflow: hidden;
-      width: 80px;
       text-align: right;
     }
 
